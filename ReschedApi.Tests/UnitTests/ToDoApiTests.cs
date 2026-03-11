597b93bcda8e994891c87374709a330215546b82
Tomasz Zapalski <tzap.media@gmail.com>
2026-03-11 12:18:07 +0000

    Added content check to GetTodosTest

--- ReschedApi.Tests/UnitTests/ToDoApiTests.cs
+++ ReschedApi.Tests/UnitTests/ToDoApiTests.cs
@@ -1,6 +1,8 @@
-﻿namespace ReschedApi.Tests;
+﻿using System.Net.Http.Json;
 
-public class BasicTests
+namespace ReschedApi.Tests;
+
+public class ToDoApiTests
 {
     private CustomWebApplicationFactory<Program>
         _factory;
@@ -28,6 +30,10 @@ public class BasicTests
 
         // Assert
         response.EnsureSuccessStatusCode(); // Status Code 200-299
-        Assert.That(response.Content.Headers.ContentType.ToString(), Is.EqualTo("application/json; charset=utf-8"));        
+        Assert.That(response.Content.Headers.ContentType?.ToString(), Is.EqualTo("application/json; charset=utf-8"));        
+        var todo = await response.Content.ReadFromJsonAsync<List<Todo>>();
+        Assert.That(todo[0].Id, Is.EqualTo(1));
+        Assert.That(todo[0].Name, Is.EqualTo("Test task"));
+        Assert.That(todo[0].IsComplete, Is.EqualTo(false));
     }
 }
